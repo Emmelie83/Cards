048162c6ed9fdf961a87c442849ec5175bcd02df
Emmelie83 <ej224sk@student.lnu.se>
2025-08-18 00:58:26 +0200

    Add RabbitMQ

--- src/test/java/com/emmeliejohansson/cards/CardsApplicationTests.java
+++ src/test/java/com/emmeliejohansson/cards/CardsApplicationTests.java
@@ -1,13 +1,8 @@
 package com.emmeliejohansson.cards;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class CardsApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
